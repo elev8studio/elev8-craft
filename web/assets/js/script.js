98f6dfa8ad444761e1352c07b2c4a5ef7c21de8d
Jonathan Smithies <jonathan@loqbox.co.uk>
2020-09-25 10:00:23 +0100

    Javascript improvements

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,42 +1,61 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const burger = document.querySelector(".burger-container");
-  const header = document.querySelector(".header");
+window.addEventListener('DOMContentLoaded', function () {
+  openMenu()
+  animateHeader()
+  darkMode()
+})
 
+/**
+ * Begin Mobile Menu
+ */
+let openMenu = () => {
+  const burger = document.querySelector('.burger-container')
   burger.onclick = () => {
-    header.classList.toggle("navigation-open");
-  };
+    let header = document.querySelector('.header')
+    header.classList.toggle('navigation-open')
+  }
+}
+/**
+ * End Mobile Menu
+ */
 
-  let previous = window.pageYOffset;
-  document.addEventListener("scroll", () => {
-    let windowOffset = window.pageYOffset;
+/**
+ * Begin Header Animation
+ */
+let animateHeader = () => {
+  let previous = window.pageYOffset
+  document.addEventListener('scroll', () => {
+    let header = document.querySelector('.header')
+    let windowOffset = window.pageYOffset
     window.pageYOffset > 50
-      ? header.classList.add("scroll")
-      : header.classList.remove("scroll"),
+      ? header.classList.add('scroll')
+      : header.classList.remove('scroll'),
       windowOffset > previous
         ? header.setAttribute(
-            "style",
-            "transform: translateY(calc(-100% + 0px)) translateZ(0px)"
-          )
-        : header.setAttribute("style", "transform: none"),
-      (previous = windowOffset <= 0 ? 0 : windowOffset);
-  });
+        'style',
+        'transform: translateY(calc(-100% + 0px)) translateZ(0px)',
+        )
+        : header.setAttribute('style', 'transform: none'),
+      (previous = windowOffset <= 0 ? 0 : windowOffset)
+  })
+}
+/**
+ * End Header Animation
+ */
 
-  const darkmodeToggle = document.querySelector("input[name=theme-toggle]");
+/**
+ * Begin Dark Mode
+ */
+let darkMode = () => {
+  const darkmodeToggle = document.querySelector('input[name=theme-toggle]')
 
-  darkmodeToggle.addEventListener("change", function () {
+  darkmodeToggle.addEventListener('change', function () {
     if (this.checked) {
-      trans();
-      document.documentElement.setAttribute("data-theme", "dark");
+      document.documentElement.setAttribute('data-theme', 'dark')
     } else {
-      trans();
-      document.documentElement.setAttribute("data-theme", "light");
+      document.documentElement.setAttribute('data-theme', 'light')
     }
-  });
-
-  let trans = () => {
-    document.documentElement.classList.add("transition");
-    window.setTimeout(() => {
-      document.documentElement.classList.remove("transition");
-    }, 1000);
-  };
-});
+  })
+}
+/**
+ * End Dark Mode
+ */
