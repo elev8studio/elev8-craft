bb9cd3416fed705b9799dc23adb9d192e128eb11
Jonathan Smithies <jonathan@loqbox.co.uk>
2020-10-08 15:48:33 +0100

    Contact form added

--- config/general.php
+++ config/general.php
@@ -47,7 +47,7 @@ return [
     // Staging environment settings
     'staging' => [
         // Set this to `false` to prevent administrative changes from being made on staging
-        'allowAdminChanges' => true,
+        'allowAdminChanges' => false,
         'allowAutoUpdates' => false,
     ],
 
---
+++ config/password-policy.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    // Minimum password length
+    "minLength" => 16,
+
+    // Maximum password length
+    "maxLength" => 160,
+
+    // Force users to use different cases
+    "cases" => true,
+
+    // Require at least 1 number
+    "numbers" => true,
+
+    // Require at least one symbol
+    "symbols" => true,
+
+    // Check the Have I been Pwned database
+    "checkPwned" => true,
+
+    // Show a password strength indicator
+    "showStrengthIndicator" => true,
+];
